3940128ef38c4ef1393e9c8b52a71513a1d47c79
ApexV2 <vojtechklep@gmail.com>
2026-03-11 16:38:58 +0100

    Wire up all commands in Program.cs with CommandDispatcher

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Winux.Core;
+using Winux.Commands;
+
+namespace Winux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dispatcher = new CommandDispatcher();
+
+            // Register all commands
+            dispatcher.RegisterCommand(new Whoami());
+            dispatcher.RegisterCommand(new Cat());
+            dispatcher.RegisterCommand(new Touch());
+            dispatcher.RegisterCommand(new Mkdir());
+
+            dispatcher.Dispatch(args);
+        }
+    }
+}
