2ad45a81cc211ed15d967f1931d5396e4ef1bbe1
ApexV2 <vojtechklep@gmail.com>
2026-03-11 16:43:45 +0100

    Register all new commands (ls, rm, cp, mv, pwd, echo, clear) in Program.cs

--- Program.cs
+++ Program.cs
@@ -10,11 +10,17 @@ namespace Winux
         {
             var dispatcher = new CommandDispatcher();
 
-            
             dispatcher.RegisterCommand(new Whoami());
             dispatcher.RegisterCommand(new Cat());
             dispatcher.RegisterCommand(new Touch());
             dispatcher.RegisterCommand(new Mkdir());
+            dispatcher.RegisterCommand(new Ls());
+            dispatcher.RegisterCommand(new Rm());
+            dispatcher.RegisterCommand(new Cp());
+            dispatcher.RegisterCommand(new Mv());
+            dispatcher.RegisterCommand(new Pwd());
+            dispatcher.RegisterCommand(new Echo());
+            dispatcher.RegisterCommand(new Clear());
 
             dispatcher.Dispatch(args);
         }
